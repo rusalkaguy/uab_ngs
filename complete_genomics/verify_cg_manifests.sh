65d93acdfd87da1fc7f44e1e15a0f0e4df41a268
U-UAB\curtish <curtish@Genome-Curtis-H.ad.uab.edu>
2013-08-29 11:39:12 -0500

    re-write to verify samples in parallel (fork/bg)

--- complete_genomics/verify_cg_manifests.sh
+++ complete_genomics/verify_cg_manifests.sh
@@ -4,15 +4,24 @@
 # Check integrety of Complete Genomics Hard Drive
 #
 
-MANIFEST_LIST=`/usr/bin/find . -name manifest.all`
-STATUS=manifest.status.txt
-echo "sample	status	ok	failed" > $STATUS
-
-for m in ${MANIFEST_LIST}; do
-	D=`dirname $m`
-	echo "--------------------------- $m ----------------------------"
-	pushd $D
-	sha256sum -c manifest.all | tee manifest.all.out
+if [ -z "$1" ]; then
+	# find and fork manifests
+	MANIFEST_LIST=`/usr/bin/find . -name manifest.all`
+	for m in ${MANIFEST_LIST}; do
+		D=`dirname $m`
+		ASM=`basename $D`
+		DD=`dirname $D`
+		DID=`basename $DD`
+		STATUS=../../$DID.$ASM.status.txt
+		echo "--------------------------- $m ----------------------------"
+		pushd $D
+		/home/curtish/uab_ngs/complete_genomics/verify_cg_manifests.sh $PWD/manifest.all $PWD/$STATUS &
+		popd 
+	done
+	wait
+else
+	# process manifest
+	sha256sum -c $1 > manifest.all.out
 	RC=$?	
 	COUNT_FAIL=`grep -c " FAILED" manifest.all.out`
 	COUNT_OK=`grep -c " OK" manifest.all.out`
@@ -21,10 +30,9 @@ for m in ${MANIFEST_LIST}; do
 	else
 		SAMPLE_STATUS="OK"
 	fi
-	echo "$D	$SAMPLE_STATUS	$COUNT_OK	$COUNT_FAIL" >> ../../$STATUS
-	popd 
-done
-
+	echo "sample	status	ok	failed" > $2
+	echo "$D	$SAMPLE_STATUS	$COUNT_OK	$COUNT_FAIL" >> $2
+fi
 	
 echo "=========================== DONE ==============================="
 
