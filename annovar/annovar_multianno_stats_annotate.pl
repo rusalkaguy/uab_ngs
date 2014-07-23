8046f364243c5116aa65b524a9adf4c9a5c10349
Vincent Laufer <vlaufer@uab.edu>
2014-07-23 14:43:03 -0500

    Added is_snp and removed is_indel to output

--- annovar/annovar_multianno_stats_annotate.pl
+++ annovar/annovar_multianno_stats_annotate.pl
@@ -68,6 +68,7 @@ while(<>) {
 	    my @header_keys = split(m/[\t\n\r]/,$_);
 	    push @header_keys, "max_aaf";
 	    push @header_keys, "is_novel";
+            push @header_keys, "is_snp";
 	    foreach my $key ( @header_keys ) {
 		$CHROM{$key} = $index; 
 		print "\t#CHROM[$index]: $key -> $index\n" if($debug);
@@ -86,11 +87,11 @@ while(<>) {
 	my @fields = split(m/[\t\n\r]/,$_);
 
 	# how do we tell if it's a INDEL?
-	my $is_indel = 0;
+	my $is_snp = 1;
 	my $ref_seq = $fields[$CHROM{Ref}]; $ref_seq =~ s/-//;
 	my $alt_seq = $fields[$CHROM{Alt}]; $alt_seq =~ s/-//;
 	if( length($ref_seq) != 1 && length($alt_seq) != 1 ) {
-	    $is_indel = 1;
+	    $is_snp = 0;
 	    $counts{indel}++;
 	} else {
 	    $counts{snp}++;
@@ -105,130 +106,4 @@ while(<>) {
 	$is_novel = 0 if( "NA" ne $snp137 || "NA" ne $snp138);
 	# compute max observer allele freq
 	foreach my $pop ( "1000g2012apr_all", "1000g2012apr_eur", "esp6500si_all", "esp6500si_ea", "cg46" ) {
-	    my $maf= $fields[$CHROM{$pop}];
-	    if( "NA" ne $maf ) { 
-		$is_novel=0; 
-		if("NA" eq $max_maf || $max_maf < $maf ) { 
-		    $max_maf=$maf; 
-		}
-	    }
-	}
-	# add max_aaf as column
-	$fields[$CHROM{"max_aaf"}] = $max_maf;
-	$fields[$CHROM{"is_novel"}] = $is_novel;
-
-	# count at levels
-	$counts{variant_maf05}++ if($max_maf <= 0.05);
-	$counts{indel_maf05}++ if($max_maf <= 0.05 && $is_indel);
-	$counts{variant_maf03}++ if($max_maf <= 0.03);
-	$counts{indel_maf03}++ if($max_maf <= 0.03 && $is_indel);
-	$counts{variant_maf01}++ if($max_maf <= 0.01);
-	$counts{indel_maf01}++ if($max_maf <= 0.01 && $is_indel);
-	# actually filter novel
-	if( !$is_novel)	{ 
-	    $counts{variant_known}++; 
-	    $counts{indel_known}++ if( $is_indel); 
-	    if( $req_novel ) {
-		print "[$linenum] DROP DATA[#col=",scalar(@fields),",is_indel=?, is_novel=$is_novel : $snp137,$max_maf\n"  if($debug);
-		next;
-	    }
-	} else {
-	    $counts{variant_novel}++ ;
-	    $counts{indel_novel}++ if( $is_indel); 
-	};
-
-	#
-	# SNP scoring
-	# see http://www.openbioinformatics.org/annovar/annovar_filter.html#ljb23
-	#
-
-	# CADD score (scaled)
-	my $cadd_phred=$fields[$CHROM{cadd}];
-	if( "NA" ne $fields[$CHROM{cadd}] ) {
-	    $cadd_obs_min = $cadd_phred if( $cadd_phred < $cadd_obs_min);
-	    $cadd_obs_max = $cadd_phred if( $cadd_phred > $cadd_obs_max);
-	} else {
-	    $cadd_phred = 0;  # replace missing data with sentinal value 0
-	}
-
-	# PolyPhen 
-	my $pphenHdiv2=$fields[$CHROM{LJB2_PolyPhen2_HDIV}];
-	
-	# SIFT
-	my $sift2=$fields[$CHROM{LJB2_SIFT}];
-	    
-
-	# eleminate if it is a SNP and fails all three impact scores. 
-	if( !$is_indel ) {
-	    my $pphen_hit = ($pphenHdiv2 > $pphenHdiv2_min)?1:0;
-	    my $sift_hit = ($sift2 <= $sift2_maxeq)?1:0;
-	    my $cadd_hit = ($cadd_phred > $cadd_min)?1:0;
-	    my $hit_key = "$cadd_hit$pphen_hit$sift_hit";
-
-	    $counts{cadd_hit}++ if( $cadd_hit);
-	    $counts{sift_hit}++ if( $sift_hit);
-	    $counts{pphen_hit}++ if( $pphen_hit);
-	    $counts{sift_and_pphen_hit}++ if( $pphen_hit && $sift_hit);
-	    $counts{sift_or_pphen_hit}++ if( $pphen_hit || $sift_hit);
-	    $counts{cadd_sift_and_pphen_hit}++ if( $cadd_hit && $pphen_hit && $sift_hit);
-	    $counts{cadd_sift_or_pphen_hit}++ if( $cadd_hit || $pphen_hit || $sift_hit);
-
-	    $and_counts{$hit_key}++;
-	    $or_counts{$hit_key}++ if( $cadd_hit || $pphen_hit || $sift_hit);
-	    #print "DROP KEY $hit_key $and_counts{$hit_key} $or_counts{$hit_key}\n";
-#	    if( ! ($pphen_hit || $sift_hit || $cadd_hit) ) {
-#		# reject snp as un-interesting
-#		print "[$linenum] DROP DATA[#col=",scalar(@fields),",is_indel=$is_indel]\n"  if($debug);
-#		next;
-#	    } else {
-		# pass as possibly interesting 
-		$counts{snp_pass}++;
-#	    }
-	}
-	# track cadd scores for pphen/sift hits
-	if( "NA" ne $fields[$CHROM{cadd}] ) {
-	    $cadd_pass_min = $cadd_phred if( $cadd_phred < $cadd_pass_min);
-	    $cadd_pass_max = $cadd_phred if( $cadd_phred > $cadd_pass_max);
-	}
-
-	print "[$linenum] PASS DATA[#col=",scalar(@fields),",is_indel=$is_indel]\n"  if($debug);
-	print join("\t",@fields),"\n";
-    }
-    
-}
-
-# summary at end
-
-print STDERR "# stats\n";
-print STDERR "# variant_count=$counts{variant}\n";
-print STDERR "#         sift2_maxeq=$sift2_maxeq\n";
-print STDERR "#         pphenHdiv2_min=$pphenHdiv2_min\n";
-print STDERR "#         cadd_min=$cadd_min\n";
-print STDERR "#     indel_count=$counts{indel}\n";
-print STDERR "#     snp_count=$counts{snp}\n";
-print STDERR "#     snp_pass=$counts{snp_pass}\n";
-foreach my $k ( sort keys %counts ) {
-    print STDERR "#         $k=$counts{$k}\n";
-}
-#print STDERR "AND=",Dumper(\%and_counts),"\n";
-#print STDERR "OR=",Dumper(\%or_counts),"\n";
-print STDERR join("\t", "#", "AND",  "cadd", "pphen", "sift", "pp+sift" ), "\n";
-print STDERR join("\t", "#", "tot",   $counts{cadd_hit},$counts{pphen_hit},$counts{sift_hit},$counts{sift_and_pphen_hit}), "\n";
-print STDERR join("\t", "#", "cadd",  $and_counts{"100"},$and_counts{"110"},$and_counts{"101"},$and_counts{"111"}), "\n";
-print STDERR join("\t", "#", "pphen", $and_counts{"110"},$and_counts{"010"},$and_counts{"011"},$and_counts{"011"}), "\n";
-print STDERR join("\t", "#", "sift",  $and_counts{"101"},$and_counts{"011"},$and_counts{"001"},$and_counts{"011"}), "\n";
-print STDERR join("\t", "#", "OR", "cadd|pphen", "cadd|sift", "pphen|sift", "cadd|pphen|sift" ), "\n";
-print STDERR join("\t", "#", "tot",
-		  $or_counts{"110"}+$or_counts{"010"}+$or_counts{"100"},
-		  $or_counts{"101"}+$or_counts{"001"}+$or_counts{"100"},
-		  $or_counts{"011"}+$or_counts{"001"}+$or_counts{"010"},
-		  $or_counts{"111"}+$or_counts{"001"}+$or_counts{"010"}+$or_counts{"100"}+$or_counts{"110"}+$or_counts{"101"}+$or_counts{"011"}
-    ), "\n";
-#print STDERR "# counts=", Dumper(\%counts),"\n";
-
-print STDERR "#\n" ;
-    print STDERR "# extra stats\n";
-print STDERR "# cadd_obs_min=$cadd_obs_min\n";
-print STDERR "# cadd_obs_max=$cadd_obs_max\n";
-print STDERR "# cadd_pass_min=$cadd_pass_min\n";
-print STDERR "# cadd_pass_max=$cadd_pass_max\n";
+	    my $maf= $fields[$
