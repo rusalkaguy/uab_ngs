1154f9268b563603dc57eb1d4485654ff6379f08
Curtis Hendrickson <curtish@uab.edu>
2022-03-01 10:37:12 -0600

    first version; hack to right justify columns

---
+++ lts/lts_du.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# lts_du [lts:/]
+#
+# concise list of s3 bucket size
+#
+
+# parse arges
+export SERVICE="lts"
+if [ ! -z "$1" ]; then export SERVICE="$1";fi
+echo "# SERVICE=$SERVICE"
+
+# get list of top-level buckets
+export BUCKETS=$(rclone lsd $SERVICE:/ | awk '{printf $5" "}')
+echo "# BUCKETS=$BUCKETS"
+
+# get sizes for all buckets
+(
+echo "BUCKET NAME OBJS BYES SIZE SIZE_U"
+( for BUCKET in $BUCKETS; do 
+    echo $(rclone size $SERVICE:/$BUCKET) |awk -v NAME=$BUCKET 'BEGIN{OFS="\t";COUNT=3;SIZE=6;SIZEU=7;BYTES=8}{B=$BYTES;sub(/[^0-9]/,"",B);print "bucket",NAME, $COUNT, B, $SIZE, $SIZEU}'
+done ) | sort -k3,3nr 
+) | rev | column -t | rev
+
+
